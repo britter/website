28ed401cde1b79365caa009c6b8d50a8b5bfa8bf
Benedikt Ritter <beneritter@gmail.com>
2025-04-01 11:53:25 +0200

    Add blog post about the 2025 OceanSprint

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import icon from "astro-icon";
 import remarkCalloutDirectives from "@microflash/remark-callout-directives";
 import githubCalloutOptions from "@microflash/remark-callout-directives/config/github";
 import remarkDirective from "remark-directive";
+import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()],
+  integrations: [tailwind(), icon(), react(), mdx()],
   redirects: {
     "/blogs": "/blog",
     "/blogs/2025-01-02-gradle-nix": "/blog/2025/01/02/gradle-nix",
